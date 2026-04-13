24b9c949c77101eae15be3e2463fabb9c851438d
ajimenez-11 <antonio6.jimenez6@gmail.com>
2026-04-13 18:25:23 +0200

    Taula receptes creada

---
+++ backend/database/migrations/2026_04_13_155354_create_receptes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('receptes', function (Blueprint $table) {
+            $table->id();
+
+            // fk cap a users
+            $table->unsignedBigInteger('usuari_id');
+            $table->foreign('usuari_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('restrict');
+
+            $table->string('nom');
+            $table->text('descripcio')->nullable();
+            $table->unsignedInteger('porcions_base')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('receptes');
+    }
+};
