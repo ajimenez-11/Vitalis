03e1b8783ea14f3ccef9700f18c666fe71ebcd2b
ajimenez-11 <antonio6.jimenez6@gmail.com>
2026-04-20 17:48:14 +0200

    fix: quantitat afegir a la Taula Lots per garantir tracabilitat

--- backend/database/seeders/LotsSeeder.php
+++ backend/database/seeders/LotsSeeder.php
@@ -11,18 +11,21 @@ class LotsSeeder extends Seeder
             'linia_albaran_id' => 1,
             'numero_lot' => 'ARR-1234',
             'data_caducitat' => '2025-03-01',
+            'quantitat' => 50.000,
         ]);
 
         Lot::create([
             'linia_albaran_id' => 2,
             'numero_lot' => 'POL-5678',
             'data_caducitat' => '2025-03-15',
+            'quantitat' => 100.000,
         ]);
 
         Lot::create([
             'linia_albaran_id' => 3,
             'numero_lot' => 'VED-9101',
             'data_caducitat' => '2025-02-20',
+            'quantitat' => 30.500,
         ]);
     }
 }
