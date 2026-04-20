071d18753cfe9eb41a8ac149c7bf10816ee9f637
ajimenez-11 <antonio6.jimenez6@gmail.com>
2026-04-20 16:21:52 +0200

    fix: LiniaAlbaranController corregido

--- backend/app/Http/Controllers/Api/LiniaAlbaranController.php
+++ backend/app/Http/Controllers/Api/LiniaAlbaranController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Albaran;
 use App\Models\LiniaAlbaran;
 
 class LiniaAlbaranController extends Controller
