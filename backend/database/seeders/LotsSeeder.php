c349ceb86bae212401a98a3c89c653a033094c12
ajimenez-11 <antonio6.jimenez6@gmail.com>
2026-04-21 17:38:50 +0200

    imp: LotSeeder actualitzat amb totes les linia_albaran per fer proves

--- backend/database/seeders/LotsSeeder.php
+++ backend/database/seeders/LotsSeeder.php
@@ -27,5 +27,26 @@ class LotsSeeder extends Seeder
             'data_caducitat' => '2026-04-20',
             'quantitat' => 20.000,
         ]);
+
+        Lot::create([
+            'linia_albaran_id' => 4,
+            'numero_lot' => 'TOM-1122',
+            'data_caducitat' => '2026-06-01',
+            'quantitat' => 40.000
+        ]);
+        
+        Lot::create([
+            'linia_albaran_id' => 5,
+            'numero_lot' => 'CEB-3344',
+            'data_caducitat' => '2026-06-15',
+            'quantitat' => 35.000
+        ]);
+
+        Lot::create([
+            'linia_albaran_id' => 6,
+            'numero_lot' => 'OLI-5566',
+            'data_caducitat' => '2026-12-31',
+            'quantitat' => 15.000
+        ]);
     }
 }
