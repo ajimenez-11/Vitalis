40a97c2f262e1da4a66c225655d8866253dcfd25
ajimenez-11 <antonio6.jimenez6@gmail.com>
2026-04-22 17:36:02 +0200

    ix: envolta l'aplicació amb AuthProvider per evitar errors de context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Auth/Login.jsx'
-import Dashboard from './pages/Dashboard/Dashboard.jsx'
-import Receptes from './pages/Receptes/ReceptesList.jsx'
+import AppRouter from "./routes/AppRouter";
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"          element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/receptes" element={<Receptes />} />
-      </Routes>
-    </BrowserRouter>
-  )
+export default function App() {
+  return <AppRouter />
 }
-
-export default App
