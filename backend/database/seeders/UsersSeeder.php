c4abbd14b24a061f1082a39887b774d683eab455
ajimenez-11 <antonio6.jimenez6@gmail.com>
2026-05-07 16:12:11 +0200

    fix: UsersSeeder amb insertOrIgnore per evitar duplicats en seeders

--- backend/database/seeders/UsersSeeder.php
+++ backend/database/seeders/UsersSeeder.php
@@ -10,7 +10,7 @@ class UsersSeeder extends Seeder
 {
     public function run(): void
     {
-        User::insert([
+        User::insertOrIgnore([
             [
                 'nom' => 'Administrador',
                 'email' => 'admin@vitalis.com',
