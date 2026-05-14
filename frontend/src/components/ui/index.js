59cb43e9080ab5e583e59c6d561114507d1f4551
ajimenez-11 <antonio6.jimenez6@gmail.com>
2026-05-14 16:37:12 +0200

    refactor: centralitza exports dels components UI

--- frontend/src/components/ui/index.js
+++ frontend/src/components/ui/index.js
@@ -1,6 +1,6 @@
-export { default as Badge }      from './Badge/Badge';
-export { default as Button }     from './Button/Button';
+export { default as Badge } from './Badge/Badge';
+export { default as Button } from './Button/Button';
 export { default as FormField }  from './FormField/FormField';
-export { default as Modal }      from './Modal/Modal';
+export { default as Modal } from './Modal/Modal';
 export { default as PageHeader } from './PageHeader/PageHeader';
-export { default as Table }      from './Table/Table';
+export { default as Table } from './Table/Table';
